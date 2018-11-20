58d9c84ab3c98577f393ead4dde0d000ee12306a
Deil <805130732@qq.com>
2018-11-20 16:27:37 +0800

    Sqlite..

--- musicservicec/src/main/java/com/example/musicservicec/MusicService.java
+++ musicservicec/src/main/java/com/example/musicservicec/MusicService.java
@@ -40,10 +40,6 @@ public class MusicService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-
-
-
-
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
